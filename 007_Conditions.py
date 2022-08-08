5207ea6fe8347a1898a89e456f61d80e6b7fee4b
sajjadshadrooz <shadruz2013@gmail.com>
2022-08-08 20:32:52 +0430

    Written on day 2.

---
+++ 007_Conditions.py
@@ -0,0 +1,20 @@
+
+# if expression 1:              < Expression: True or False >
+#     command 1                 < Tasks when expression 1 is True  >
+# elif expression 2:            < Expression 2: True or False >
+#     command 2                 < Tasks when expression 2 is True  >
+# else:                         < Default >
+#     command 3                 < Default tasks >
+
+######################################################################
+
+#  Command1  if expression  else Command2
+
+######################################################################
+
+#  The == Operator ===> Values are equal.
+#  The is Operator ===> Point to same object.
+
+######################################################################
+
+# Falsyness =>  empty objects , zero or 0 , empty strings , None , False
